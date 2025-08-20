5257264116c179087682447127a4f361b94364ed
SooooCoding <poomisujin05@gmail.com>
2025-08-20 13:55:02 +1200

    Create Customer Database Table & Node.js Database Connection Setting

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const fs = require('fs');
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 5000;
@@ -5,33 +6,27 @@ const port = process.env.PORT || 5000;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+const data = fs.readFileSync('./database.json');
+const conf = JSON.parse(data);
+const mysql = require('mysql');
+
+const connection = mysql.createConnection({
+    host: conf.host,
+    user: conf.user,
+    password: conf.password,
+    port: conf.port,
+    database: conf.database
+});
+
+connection.connect();
+
 app.get('/api/customers', (req, res) => {
-    res.send([
-        {
-        'id': 1,
-        'image': 'https://randomuser.me/api/portraits/men/1.jpg',
-        'name': 'John Smith',
-        'birthday': '22/Dec/1996',
-        'gender' : 'Male',
-        'job' : 'Student'
-        },
-        {
-        'id': 2,
-        'image': 'https://randomuser.me/api/portraits/women/1.jpg',
-        'name': 'Emma Wilson',
-        'birthday': '15/Aug/1995',
-        'gender' : 'Female',
-        'job' : 'Software Developer'
-        },
-        {
-        'id': 3,
-        'image': 'https://randomuser.me/api/portraits/men/2.jpg',
-        'name': 'James Brown',
-        'birthday': '03/Apr/1997',
-        'gender' : 'Male',
-        'job' : 'Graphic Designer'
+    connection.query(
+        "SELECT * FROM CUSTOMER",
+        (err, rows, fields) => {
+        res.send(rows);
         }
-    ]);
+    );
 });
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
